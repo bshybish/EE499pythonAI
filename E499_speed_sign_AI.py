008acd31d97e28c7587ad6501db655ead87ddd7d
bshybish <bassamkau@gmail.com>
2020-04-26 02:36:48 +0300

    Update E499_speed_sign_AI.py

--- E499_speed_sign_AI.py
+++ E499_speed_sign_AI.py
@@ -7,6 +7,15 @@ Created on Sun Apr 26 00:50:17 2020
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np 
+from PIL import Image as img
+from os import listdir
+from os.path import isfile, join
 
+mypath = "C:\\Users\\Bassam\\Documents\\training_data\\first_set\\"
+
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+filooos = listdir(mypath)
+s_filooos = sorted(filooos)
 
 
