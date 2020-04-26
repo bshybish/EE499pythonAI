5862b110ffbc0ff1661193d82dc3f733e9630522
bshybish <bassamkau@gmail.com>
2020-04-26 05:04:20 +0300

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -11,7 +11,7 @@ mnist = tf.keras.datasets.mnist
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
-
+plt.imshow(x_test[112], cmap= plt.cm.binary)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation= tf.nn.relu))
@@ -19,8 +19,8 @@ model.add(tf.keras.layers.Dense(128, activation= tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation= tf.nn.softmax))
 
 model.compile(optimizer='adam',
-              loss     ='sparse_categorical_crossentropy',
-              matrices =['accuracy'])
+              loss     ='sparse_categorical_crossentropy'
+              )
 
 history = model.fit(x_train, y_train, epochs=5)
 
