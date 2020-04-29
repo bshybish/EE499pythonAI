6c3740c3df8105f088974888667c784eb4722489
them0e <44075972+them0e@users.noreply.github.com>
2020-04-30 02:03:42 +0300

    Update E499_speed_sign_AI.py

--- E499_speed_sign_AI.py
+++ E499_speed_sign_AI.py
@@ -14,17 +14,7 @@ from os.path import isfile, join
 from skimage import io, filters
 
 
-def shuffle_c(xx_test, yy_test):
-    tempRand = np.arange(34)
-    tempRand = np.random.permutation(tempRand)
-    j = 0
-    for k in tempRand:
-        (xx_test[k,:,:], xx_test[j,:,:]) = (xx_test[j,:,:], xx_test[k,:,:])
-        (yy_test[k], yy_test[j]) = (yy_test[j], yy_test[k])
-        j = j + 1
-    return (xx_test, yy_test)
-    
-    
+
 #%%
 
 
@@ -35,27 +25,38 @@ t_files         = listdir(mypath)
 y_test = np.empty(519)
 
 for i in range(519):
-    if (i<=105):
+    if (i<=61):
+        y_test[i]=100
+    if (i>61 and i<=167):
         y_test[i]=30
-    if (i>105 and i<=198):
+    if (i>167 and i<=260):
         y_test[i]=40
-    if (i>198 and i<=288):
+    if (i>260 and i<=350):
         y_test[i]=50
-    if (i>288 and i<=377):
-        y_test[i]=60
-    if (i>377 and i<=456):
+    if (i>377 and i<=439):
         y_test[i]=80
-    if (i>456 and i<=519):
+    if (i>439 and i<=519):
         y_test[i]=100
         
 #y_test          = [30, 40, 30, 30, 40, 50, 50, 50, 70, 80, 80, 80, 80,
 #                   80, 80, 80, 80, 80, 100, 100, 80, 90, 90,
 ##                   90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
-x_test          = np.zeros((0,32,32))
+x_test          = np.zeros((0,128,128))
 
+def shuffle_c(xx_test, yy_test):
+    tempRand = np.arange(128)
+    tempRand = np.random.permutation(tempRand)
+    j = 0
+    for k in tempRand:
+        (xx_test[k,:,:], xx_test[j,:,:]) = (xx_test[j,:,:], xx_test[k,:,:])
+        (yy_test[k], yy_test[j]) = (yy_test[j], yy_test[k])
+        j = j + 1
+    return (xx_test, yy_test)
+    
+    
 for i in t_files:
     x4 = cv2.imread(mypath + i, cv2.IMREAD_GRAYSCALE)
-    x4 = cv2.resize(x4, (32,32))
+    x4 = cv2.resize(x4, (128,128))
     x4 = tf.keras.utils.normalize(x4)
     x_test = np.append(x_test,[x4],axis=0)
     
